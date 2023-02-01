4f2d6091ecb4cb675d40d845c1363b8d47f91303
maxrainy <115626270+Maxrainyx@users.noreply.github.com>
2023-02-01 18:05:20 +0300

    fixing db

--- Sprint/fstr/models.py
+++ Sprint/fstr/models.py
@@ -9,7 +9,6 @@ class PerevalAdded(models.Model):
     name = models.CharField(max_length=100)
     date_added = models.DateTimeField(auto_now_add=True)
     raw_data = JSONField()
-    images = JSONField()
     bTitle = models.CharField(max_length=10)
     title = models.CharField(max_length=100)
     other_title = models.CharField(max_length=100)
@@ -19,20 +18,11 @@ class PerevalAdded(models.Model):
     summer = models.CharField(max_length=100)
     autumn = models.CharField(max_length=100)
     winter = models.CharField(max_length=100)
-
-
-class PerevalAreas(models.Model):
-    """" # TODO """
-    id_parent = ...  # TODO
-    title = models.TextField(max_length=100)
+    pereval_images = models.ManyToManyField('PerevalImages', through='Images')
 
 
 class Activities(models.Model):
-    ...
     title = models.CharField(max_length=100)
-
-
-unique = True
 
 
 class Coords(models.Model):
@@ -46,40 +36,10 @@ class Coords(models.Model):
 
 class PerevalImages(models.Model):
     time_added = models.DateTimeField(auto_now_add=True)
-    img = models.B
+    img = models.BinaryField(null=True)
 
 
-...
+class Images(models.Model):
+    pereval_image = models.ManyToManyField(PerevalImages)
+    pereval_added = models.ForeignKey(PerevalAdded, on_delete=models.CASCADE)
 
-
-class Post(models.Model):
-    """ Post model with 'title', 'text' fields """
-    title = models.CharField(max_length=255)  # title column
-    text = RichTextUploadingField()  # text column
-    created = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='post_category')  # o-t-o - Category
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')  # o-t-o with User
-
-    def __str__(self):
-        return f'{self.title}'
-
-    def get_absolute_url(self):
-        """ Method for the redirection """
-        return reverse('post_detail', args=[str(self.id)])
-
-
-class Comment(models.Model):
-    """ Comment model with 'text' fields and One-To-One relationship with the Post model """
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user')
-    text = models.TextField(max_length=255)  # simple text field to contain the comment
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment_post')  # o-t-o with Post
-    rating = models.IntegerField(default=0)  # rating of the comment, starts from 0 initially
-    created = models.DateTimeField(auto_now_add=True)  # time-field with automatically managed creation time
-    approved = models.BooleanField(default=False)  # to check whether comment approved by the post author or not
-
-    def __str__(self):
-        return f'{self.text}'
-
-    def get_absolute_url(self):
-        """ Method for the redirection """
-        return reverse('post_detail', args=[str(self.post_id)])
--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-def main():
-    ...
+class Post(models.Model):
+    """ Post model with 'title', 'text' fields """
+    title = models.CharField(max_length=255)  # title column
+    text = RichTextUploadingField()  # text column
+    created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='post_category')  # o-t-o - Category
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')  # o-t-o with User
 
+    def __str__(self):
+        return f'{self.title}'
+
+    def get_absolute_url(self):
+        """ Method for the redirection """
+        return reverse('post_detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    """ Comment model with 'text' fields and One-To-One relationship with the Post model """
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment_user')
+    text = models.TextField(max_length=255)  # simple text field to contain the comment
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comment_post')  # o-t-o with Post
+    rating = models.IntegerField(default=0)  # rating of the comment, starts from 0 initially
+    created = models.DateTimeField(auto_now_add=True)  # time-field with automatically managed creation time
+    approved = models.BooleanField(default=False)  # to check whether comment approved by the post author or not
+
+    def __str__(self):
+        return f'{self.text}'
+
+    def get_absolute_url(self):
+        """ Method for the redirection """
+        return reverse('post_detail', args=[str(self.post_id)])
